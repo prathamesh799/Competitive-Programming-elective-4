b90a5fc2a47b70cb5f7008c0f82ca91c0488a496
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-11 17:57:39 +0530

    stacks implementation on LL

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -26,21 +26,28 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
+        # new = Element(new_element)
+        new_element.next = self.head
+        self.head = new_element
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        if not self.head:
+            return None
+        temp = self.head.next
+        val = self.head
+        self.head = temp
+        return val
 
 class stack(object):
     def __init__(self,top=None):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
-        "Push (add) a new element onto the top of the stack"
-        pass
+        # new = Element(new_element)
+        self.ll.insert_first(new_element)
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        return self.ll.delete_first()
     
