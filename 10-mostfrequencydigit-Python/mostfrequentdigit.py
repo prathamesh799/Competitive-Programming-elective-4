d244606997fec7a19dde0ed1fd3f05ff4ebea9a5
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-11 21:10:41 +0530

    most frequent digit with dict and iteration

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -3,5 +3,21 @@
 # that occurs most frequently in it, with ties going to the smaller digit.
 
 def mostfrequentdigit(n):
-	# your code goes here
-	pass
+	d = {}
+	while n > 0:
+		rem = n % 10
+		d[rem] = d.get(rem, 0) + 1
+		n //= 10
+	# print(d)
+	mc = -1
+	md = 0
+	for k, v in d.items():
+		if v > mc:
+			mc = v
+			md = k
+		elif v == mc:
+			md = md if md < k else k
+		# print(md)
+	return md
+
+print(mostfrequentdigit(26011))
