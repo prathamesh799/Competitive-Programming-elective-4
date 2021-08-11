460573abb454ed1f7b93769b2a7d10db187f1a0d
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-11 16:05:52 +0530

    pascals triangle by constructing a triangle

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -5,8 +5,21 @@
 # each row starts at column 0. If either row or col 
 # are not legal values, return None, instead of crashing. 
 
-
-
+def buildTriangle(r):
+	r += 1
+	t = [[0 for i in range(r)] for i in range(r)]
+	for i in range(r):
+		t[i][0] = 1
+	for i in range(1,r):
+		for j in range(r):
+			t[i][j] = t[i-1][j] + t[i-1][j-1]
+	return t
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	if col > row:
+		return 0
+	triangle = buildTriangle(row)
+	# print(triangle)
+	return triangle[row][col]
+
+# print(fun_pascaltrianglevalue(7,7))
