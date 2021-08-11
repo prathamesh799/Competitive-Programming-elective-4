8c400906487510d23f6df19cfeaf4a6200280f24
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-11 22:45:12 +0530

    quicksort not implemented

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -3,4 +3,23 @@ Input a list.
 Output a sorted list."""
 def quicksort(array):
 	# Your code goes here
-	pass
+	l = len(array)
+	for i in range(l):
+		pivot = array[i]
+		l, h = i + 1, l-1
+		while l < h:
+			while l < i and array[l] <= pivot:
+				l += 1
+			while h > i and array[h] > pivot:
+				h -= 1
+			if l < h:
+				array[l], array[h] = array[h], array[l]
+			# else:
+			# 	if l < i:
+			# 		array[i], array[l] = array[l], array[i]
+			# 	if h > i:
+			# 		array[i], array[h] = array[h], array[i]
+			l += 1
+			h -= 1
+			print(pivot, array)
+print(quicksort([21, 4, 1, 3, 9, 20, 25, 6, 21, 14]))
