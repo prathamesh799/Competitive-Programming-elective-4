3d95678cfd0cca63901029631b952b1a1f521325
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-12 19:01:54 +0530

    get kth digit v1

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -5,5 +5,15 @@
 
 
 
-def fun_get_kth_digit(digit, k):
-	return 0
+def fun_get_kth_digit(n, k):
+	n = -n if n < 0 else n
+	while k > 0 and n > 0:
+		n //= 10
+		k -= 1
+	return n % 10 if n > 0 else 0
+
+# print(fun_get_kth_digit(234,2)) # 2
+# print(fun_get_kth_digit(234,3)) # 0
+# print(fun_get_kth_digit(234,0)) # 4
+# print(fun_get_kth_digit(-234,3)) # 0
+# print(fun_get_kth_digit(-234,1)) # 3
