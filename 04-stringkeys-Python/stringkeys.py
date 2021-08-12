88f1709ca4b0a33a6c205569925cc03d0f6b73bd
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-12 20:03:32 +0530

    failing one test case

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -11,19 +11,20 @@ class HashTable(object):
         the table."""
         # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
         # Your code goes here
-        pass
+        ind = self.calculate_hash_value(string)
+        self.table[ind] = string
         
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
         # Your code goes here
-        pass
+        ind = self.calculate_hash_value(string)
+        res = self.table[ind]
+        return ind if res != None else -1
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
         # Your code goes here
-        pass
-
-
+        return ord(string[0]) * 100 + (ord(string[1]))
