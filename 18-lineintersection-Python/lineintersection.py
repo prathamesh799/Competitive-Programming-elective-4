c86920eefac740b73b3f22eb5f81076a560b3164
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-12 19:39:19 +0530

    line intersection brute forced

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -1,9 +1,12 @@
 # lineintersection(m1, b1, m2, b2)[5pts]
 # Write the function lineintersection(m1, b1, m2, b2) that takes four int or float values representing the 2 lines:
-#     y = m1*x + b1
+#     y = m1*x + b1 => m1*x -y + b1 = 0
 #     y = m2*x + b2
 # This function returns the x value of the point of intersection of the two lines. If the lines are parallel, or identical, the function should return None.
 
 def lineintersection(m1, b1, m2, b2):
 	# your code goes here
-	pass
+	if m1 == m2:
+		return None
+	a = (b2-b1) / (m1-m2)
+	return a if abs(a) != 1.0 else None
