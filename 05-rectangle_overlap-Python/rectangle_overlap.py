44e937cbffadb9a69bc6fd94b67f5467a078dc21
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-20 19:47:11 +0530

    checked by checking false conditions

--- 05-rectangle_overlap-Python/rectangle_overlap.py
+++ 05-rectangle_overlap-Python/rectangle_overlap.py
@@ -7,7 +7,12 @@
 # the x-coordinate goes up while you head right, the y-coordinate goes up while you 
 # head down (so we say that "up is down")
 
-def fun_rectangle_overlap(left1, top1, width1, height1, left2, top2, width2, height2):
-    return True
+# def getCoords(left1, top1, width1, height1):
+#     return 0, 0, 
 
-        
+def fun_rectangle_overlap(left1, top1, width1, height1, left2, top2, width2, height2):
+    if left2 > left1 + width1 or top2 > top1 + height1:
+        return False
+    if left1 > left2 + width2 or top1 > top2 + height2:
+        return False
+    return True
