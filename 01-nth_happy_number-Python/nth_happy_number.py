f0413669e99b31d7e745db5c374278620f321407
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-20 20:03:15 +0530

    used code from nth happy prime

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -14,6 +14,37 @@
 # assert(nth_happy_number(7) == 28)
 # assert(nth_happy_number(8) == 31)
 
+def isHappy(num):
+	s = num
+	if num == 7 or num == 1:
+		return True
+	while s > 9:	
+		s = 0
+		while num > 0:
+			s += (num % 10) ** 2
+			num //= 10
+		num = s
+	return s == 1
 
 def nth_happy_number(n):
-	return 0
+	num = 0
+	n -= 1
+	while n > -1:
+		if isHappy(num) == True:
+			n -= 1
+		num += 1
+	return num - 1
+
+# print(nth_happy_number(1))
+# print(nth_happy_number(2))
+# print(nth_happy_number(3))
+
+
+# assert(nth_happy_number(1) == 1)
+# assert(nth_happy_number(2) == 7)
+# assert(nth_happy_number(3) == 10)
+# assert(nth_happy_number(4) == 13)
+# assert(nth_happy_number(5) == 19)
+# assert(nth_happy_number(6) == 23)
+# assert(nth_happy_number(7) == 28)
+# assert(nth_happy_number(8) == 31)
