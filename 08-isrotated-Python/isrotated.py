83d809f306a53bd75c479f12adfb2b65a23aff8f
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-20 20:26:34 +0530

    implemented O(2N) solution

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -3,7 +3,23 @@
 # if given String is "XYZ" and "ZXY" then your function should return true, but if the input is 
 # "XYZ" and "YXZ" then return false.
 
+# O(2N) solution
+def getIndex(s2, ch):
+	for i in range(len(s2)):
+		if s2[i] == ch:
+			return i
+	return -1
 
 def isrotated(str1, str2):
-	#Your code goes here
-	pass
+	i = 0
+	j = getIndex(str2, str1[0])
+	if j == -1:
+		return False
+	for i in range(len(str1)):
+		if str1[i] != str2[j]:
+			return False
+		j += 1
+		if j == len(str2):
+			j = 0
+	return True
+		
