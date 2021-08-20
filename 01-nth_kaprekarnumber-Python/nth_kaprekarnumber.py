efa0c5093a3266a5f3e4b1d7782182f410b16ed8
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-20 23:11:14 +0530

    kaprekar by greedy approach

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -8,6 +8,46 @@
 
 
 import math
+def getSplits(n):
+    n = str(n)
+    res = []
+    for i in range(1, len(n)):
+        s1 = int(n[:i])
+        s2 = int(n[i:])
+        # while s1 != 0 and s1 % 10 == 0:
+        #     s1 //= 10
+        # while s2 != 0 and s2 % 10 == 0:
+        #     s2 //= 10
+        if s1 == 0 or s2 == 0 or (s1, s2) in res:
+            continue
+        res.append((s1, s2))
+    return res
+
+def isKaprekar(n):
+    if n == 1:
+        return True
+    s = n ** 2
+    splits = getSplits(s)
+    for each in splits:
+        if each[0] + each[1] == n:
+            return True
+    return False
 
 def fun_nth_kaprekarnumber(n):
-    return 1;
+    num = 1
+    n += 1
+    while n > 0:
+        if isKaprekar(num):
+            n -= 1
+        num += 1
+    return num - 1
+
+# print(getSplits(2025))
+# print(getSplits(1000))
+# print(getSplits(1))
+# print(isKaprekar(1))
+# print(isKaprekar(9))
+# print(isKaprekar(45))
+# print(isKaprekar(55))
+# print(fun_nth_kaprekarnumber(1))
+# print([fun_nth_kaprekarnumber(i) for i in range(21)]) 
