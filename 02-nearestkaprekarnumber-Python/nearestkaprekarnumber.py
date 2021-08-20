511edd2eaff39686ebc25348bf466b8438ab953a
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-20 23:51:24 +0530

    reused isKaprekar func from nthKaprekarNumber

--- 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
+++ 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
@@ -8,9 +8,34 @@
 # as that will not be efficient enough to get past the autograder. 
 # Hint: one way to solve this is to start at n and grow in each direction until you find a Kaprekar number.
 
-
-
 import math
 
+def getSplits(n):
+    n = str(n)
+    res = []
+    for i in range(1, len(n)):
+        s1 = int(n[:i])
+        s2 = int(n[i:])
+        if s1 == 0 or s2 == 0 or (s1, s2) in res:
+            continue
+        res.append((s1, s2))
+    return res
+
+def isKaprekar(n):
+    if n == 1:
+        return True
+    s = n ** 2
+    splits = getSplits(s)
+    for each in splits:
+        if each[0] + each[1] == n:
+            return True
+    return False
+
 def fun_nearestkaprekarnumber(n):
-    return 1
+    diff = 1
+    while True:
+        if isKaprekar(n - diff):
+            return n - diff
+        elif isKaprekar(n + diff):
+            return n + diff
+        diff += 1
