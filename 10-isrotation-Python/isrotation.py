78827fdd3835b6e60814da63eb7a51733ecb532c
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-24 15:18:10 +0530

    12345-54321 are not rotations, mistake in test case

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -6,5 +6,24 @@
 # is a rotation of itself.
 
 def isrotation(x, y):
-	# Your code goes here
-	pass
+	if x == y:
+		return True
+	dig_count = 0
+	
+	temp = x
+	while temp > 0:
+		dig_count += 1
+		temp //= 10
+	dig_count_temp = dig_count
+
+	while dig_count_temp > 0:
+		rem = x % 10
+		x //= 10
+		x += rem * 10**(dig_count-1)
+		if x == y:
+			return True
+		dig_count_temp -= 1
+		print(x)
+	return False 
+		
+# print(isrotation(12345,54321))
