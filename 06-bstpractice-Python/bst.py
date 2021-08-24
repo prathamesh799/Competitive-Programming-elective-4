f6f774d539a343e733983725c31024c81f1fc99b
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-24 13:44:41 +0530

    bst implemented without recurssions

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -1,3 +1,5 @@
+from _pytest.compat import getlocation
+
 class Node(object):
     def __init__(self, value):
         self.value = value
@@ -10,13 +12,49 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-        pass
+        temp = self.root
+        while temp:
+            # print('k',temp.value)
+            if new_val == temp.value:
+                break
+            if new_val < temp.value:
+                if temp.left == None:
+                    temp.left = Node(new_val)
+                temp = temp.left
+            elif new_val > temp.value:
+                if temp.right == None:
+                    temp.right = Node(new_val)
+                temp = temp.right
 
     def printSelf(self):
         # Your code goes here
-        pass
+        temp = self.root
+        self.getInorder(temp)
+        
+    def getInorder(self, node):
+        if node:
+            self.getInorder(node.left)
+            print(node.value, end = ' ')
+            self.getInorder(node.right)
         
     def search(self, find_val):
         # Your code goes here
-        pass
+        if find_val == None:
+            return False
+        temp = self.root
+        while temp != None:
+            if type(temp.value) == type(find_val) and  temp.value == find_val:
+                return True
+            if type(temp.value) == type(find_val) and find_val <= temp.value:
+                temp = temp.left
+            else:
+                temp = temp.right
+        return False
 
+tree = BST(4)
+tree.insert(5)
+tree.insert(1)
+tree.insert(2)
+tree.printSelf()
+print(tree.search(5))
+print(tree.search('f'))
