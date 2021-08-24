582885accac6b93d367907819709a4d79287e93c
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-24 13:58:22 +0530

    done, test cases didn't run but output was checked manually to be correct

--- 04-nth_carolprime-Python/nth_carolprime.py
+++ 04-nth_carolprime-Python/nth_carolprime.py
@@ -7,7 +7,33 @@
 # will return a 12-digit answer! In particular, this means you cannot just edit isPrime. 
 # Hint: you may need to generate only Carol numbers, and then test those as you go 
 # for primality (and you may need to think about that hint for a while for it to make sense!).
+from _pytest.compat import getlocation
 
+def isPrime(n):
+	if n < 2:
+		return False
+	if n == 2 or n == 3:
+		return True
+	if n % 2 == 0:
+		return False
+	for i in range(3, int(n**0.5)+1):
+		if n % i == 0:
+			return False
+	return True
+
+def isCarolPrime(n):
+    return isPrime(n)
 
 def fun_nth_carolprime(n):
-    return 0
+    k = 2
+    check = 0
+    while n > 0:
+        check = ((2**k - 1)**2 - 2)
+        if isCarolPrime(check):
+            n -= 1
+        k += 1
+    return check
+
+# print(isPrime(959))
+# for i in range(5):
+#     print(fun_nth_carolprime(i))
