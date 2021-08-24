80b20d70e9910f18bf33f00ff5782503e1326821
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-24 16:00:30 +0530

    used a few helper methods, all recurssive

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,5 +8,35 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
+def getDigitCount(n, c):
+	if n <= 0:
+		return c
+	c += 1
+	return getDigitCount(n//10, c)
+
+def rev(n, r, dig_cnt):
+	if n <= 0:
+		return r
+	rem = n % 10
+	n //= 10
+	r += rem * (10 ** (dig_cnt-1))
+	return rev(n, r, dig_cnt-1)
+
+def rec_even(n, r):
+	if n <= 0:
+		return r
+	rem = n % 10
+	if rem % 2 == 0:
+		r = r * 10 + rem
+	return rec_even(n//10, r)
+
+def rec_alternating(l, i, res):
+	if i == len(l):
+		return res
+	ans = rec_even(l[i], 0)
+	ans_rev = rev(ans, 0, getDigitCount(ans, 0))
+	res.append(ans_rev)
+	return rec_alternating(l, i+1, res)
+
 def fun_recursion_onlyevendigits(l): 
-		return []
+		return rec_alternating(l, 0, []) # 0 is index, [] is result list
