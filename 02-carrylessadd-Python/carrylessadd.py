aad5535a49a58e0919ae276190b178ee735f0fa8
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-25 11:52:11 +0530

    done

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -6,5 +6,15 @@
 
 
 def fun_carrylessadd(x, y):
-	return 0
+	res = 0
+	p = 0
+	while x > 0 or y > 0:
+		rem1 = x % 10 if x > 0 else 0
+		rem2 = y % 10 if y > 0 else 0
+		s = x + y if x + y < 10 else (x + y) % 10
+		res = (s * 10 ** p) + res
+		p += 1
+		x //= 10
+		y //= 10
+	return res
 
