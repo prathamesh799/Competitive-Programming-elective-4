03b9cdf25f8f9a645257e97536c9af1a922a3e38
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-25 20:31:04 +0530

    for 2 test cases output order is incorrect

--- 08-getallpermutations-Python/getallpermutations.py
+++ 08-getallpermutations-Python/getallpermutations.py
@@ -1,7 +1,21 @@
 # getallPermutations(str) [20 pts]
-# Write an efficient program to print all permutations of a given String. For example, if given input is "abc" then 
-# your program should print all 6 permutations e.g. [('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
+# Write an efficient program to print all permutations of a given String. 
+# For example, if given input is "abc" then 
+# your program should print all 6 permutations 
+# e.g. [('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
+
+def rec_perm(s, i, l, res):
+	if i == l:
+		res.append(tuple(s))
+	else:
+		for j in range(i, l):
+			s[i], s[j] = s[j], s[i]
+			rec_perm(s, i+1, l, res)
+			s[i], s[j] = s[j], s[i]
 
 def getallpermutations(x):
 	# Your code goes here
-	pass
+	res = []
+	s = list(sorted(x))
+	rec_perm(s, 0, len(x), res)
+	return sorted(res)
