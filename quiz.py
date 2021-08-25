af857d72546702fb94e4696ed0e212ade8e9f843
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-25 19:48:55 +0530

    done

--- quiz.py
+++ quiz.py
@@ -1,12 +1,40 @@
-# Write the function bestQuiz(a), which takes a rectangular 2d list of numbers that represents a gradebook, where each column represents a quiz, and each row represents a student, and each value represents that student's score on that quiz (except -1 indicates the student did not take the quiz). For example:
+# Write the function bestQuiz(a), which takes a rectangular 
+# 2d list of numbers that represents a gradebook, 
+# where each column represents a quiz, and each row represents a student, 
+# and each value represents that student's score on that quiz 
+# (except -1 indicates the student did not take the quiz). For example:
 #   a = [ [ 88,  80, 91 ],
 #         [ 68, 100, -1 ]
 #       ]
-# This list indicates that student0 scored 88 on quiz0, 80 on quiz1, and 91 on quiz2. Also, student1 scored 68 on quiz0, 100 on quiz1, and did not take quiz2. The function returns the quiz with the highest average. In this case, quiz0 average is 78, quiz1 average is 90, and quiz2 average is 91 (since we ignore the -1). Thus, quiz2 is the best, and so the function returns 2 in this case. You are not responsible for malformed input, except you should return None if there are no quizzes. Also, resolve ties in favor of the lower quiz number. Here is a test function for you:
+# This list indicates that student0 scored 88 on quiz0, 
+# 80 on quiz1, and 91 on quiz2. Also, student1 scored 68 on quiz0, 
+# 100 on quiz1, and did not take quiz2. The function returns the
+#  quiz with the highest average. In this case, quiz0 average is 78, 
+# quiz1 average is 90, and quiz2 average is 91 (since we ignore the -1). 
+# Thus, quiz2 is the best, and so the function returns 2 in this case. 
+# You are not responsible for malformed input, except you should return
+# None if there are no quizzes. Also, resolve ties in favor of the lower quiz number.
+#  Here is a test function for you:
 
 def bestQuiz(l):
-    # Your  code goes ehre...
-    pass
+    m_avg = 0
+    m_avg_ind = -1
+    for i in range(len(l[0])):
+        q_sum = 0
+        rows = 0
+        for each in range(len(l)):
+            if l[each][i] != -1:
+                q_sum += l[each][i]
+                rows += 1
+        # print(q_sum)
+        q_avg = q_sum // rows if q_sum != 0 else 0
+        # print(q_avg)
+        if q_avg > m_avg:
+            m_avg = q_avg
+            m_avg_ind = i
+        m_avg = max(m_avg, q_avg)
+    return m_avg_ind if m_avg_ind != -1 else None
+
 
 def testBestQuiz():
     print('Testing bestQuiz()...', end='')
@@ -23,3 +51,5 @@ def testBestQuiz():
           [-1, -1, -1 ]]
     assert(bestQuiz(a) == None)
     print('All test cases passed...!')
+
+print(testBestQuiz())
