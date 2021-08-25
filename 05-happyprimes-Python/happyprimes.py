9bc7344bbabd96dc7f617322e330cb98f4bfc710
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-25 12:38:31 +0530

    test case for 833 is wrong

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -9,6 +9,31 @@
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
 # functions to make nthHappyNumber(n) easier to write!
+
+def isHappy(num):
+	s = num
+	if num == 7:
+		return True
+	while s > 9:	
+		s = 0
+		while num > 0:
+			s += (num % 10) ** 2
+			num //= 10
+		num = s
+	return s == 1
+
+def isPrime(n):
+	if n < 2:
+		return False
+	if n == 2 or n == 3:
+		return True
+	if n % 2 == 0:
+		return False
+	for i in range(3, int(n**0.5)+1):
+		if n % i == 0:
+			return False
+	return True
+
 def ishappyprimenumber(n):
     # Your code goes here
-    pass
+    return isPrime(n) and isHappy(n)
