dad99abf755b5a91cadabe31b33b6125433a30d2
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-26 21:13:01 +0530

    iterative method checked test cases with n = 3, 4, 5

--- makeMagicSquare.py
+++ makeMagicSquare.py
@@ -35,4 +35,31 @@
 
 def makeMagicSquare(n):
     # Your code goes here...
-    pass
+    res = [[0 for i in range(n)] for j in range(n)]
+    i, j = n//2, n-1
+    num = 1
+    # base condition
+    res[i][j] = num
+    while num < n*n:
+        # condition 1
+        num += 1
+        i = i-1 if i != 0 else n-1
+        j = j+1 if j != n-1 else 0
+        # condition 2
+        if res[i][j] != 0:
+            i += 1
+            j -= 2
+            # condition 3
+            if i == -1 and j == n:
+                i = 0
+                j = n-2
+        i = i % n
+        j = j % n
+        res[i][j] = num
+    return res
+
+print('starting tests...')
+assert(makeMagicSquare(3) == [[2, 7, 6], [9, 5, 1], [4, 3, 8]])
+assert(makeMagicSquare(4) == [[8, 3, 14, 9], [2, 13, 12, 7], [16, 11, 6, 1], [10, 5, 4, 15]])
+assert(makeMagicSquare(5) == [[9, 3, 22, 16, 15], [2, 21, 20, 14, 8], [25, 19, 13, 7, 1], [18, 12, 6, 5, 24], [11, 10, 4, 23, 17]])
+print('passed!!')
