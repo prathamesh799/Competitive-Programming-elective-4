cf3bed63eed21a528068590d0e4e9e2d8afa8d59
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-26 19:48:58 +0530

    done

--- 11-findzerowithbisection-Python/findzerowithbisection.py
+++ 11-findzerowithbisection-Python/findzerowithbisection.py
@@ -1,12 +1,22 @@
 # Bisection Algorithm comes into play!
 # We know that the square root of x lies between 1 and x, from mathematics
-# Rather than exhaustively trying things starting at 1, suppose instead we pick a number in the middle of this range
+# Rather than exhaustively trying things starting at 1, 
+# suppose instead we pick a number in the middle of this range
 # Bisection search works when value of function varies monotonically with input
-# If g, the users input/guess, is less than/greater than the midpoint of the range, then that number becomes the new high point of said range and is then factored into the new search.
+# If g, the users input/guess, is less than/greater than the midpoint of the range, 
+# then that number becomes the new high point of said range and is then factored into the new search.
 
-def findzerowithbisection(x, epsilon):
-	pass
-	# epsilon and step are initialized
-	# don't change these values
-	# epsilon
-	# your code starts here
+"""
+1 2 3 4 5 6 7 8 9 10
+"""
+def findzerowithbisection(x,epsilon):
+    low = 0
+    high = max(1.0,x)
+    y = (high+low)/2.0
+    while abs(y**2-x)>=epsilon:
+        if y**2 < x:
+            low = y
+        else:
+            high = y
+        y = (high+low)/2.0
+    return y
