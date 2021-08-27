9a80799c43cf42ed303e7dc5c3093131ec02807a
prathamesh799 <prathameshk612@msitprogram.net>
2021-08-27 17:50:13 +0530

    done

--- 07-graphrepresentation-Python/Graph_Representation.py
+++ 07-graphrepresentation-Python/Graph_Representation.py
@@ -6,49 +6,72 @@ class Node(object):
 '''
 This class defines an Edge
 '''
+
 class Edge(object):
     def __init__(self, value, node_from, node_to):
         self.value = value
         self.node_from = node_from
         self.node_to = node_to
-
+         
 class Graph(object):
-    def __init__(self, nodes=[], edges=[]):
-        self.nodes = nodes
-        self.edges = edges
-
+    def __init__(self, nodes=None, edges=None):
+        self.nodes = nodes or []
+        self.edges = edges or []
+        self.node_names = []
+        self._node_map = {}
+ 
+ 
     def insert_node(self, new_node_val):
         new_node = Node(new_node_val)
         self.nodes.append(new_node)
-        
+        self._node_map[new_node_val] = new_node
+        return new_node
+ 
     def insert_edge(self, new_edge_val, node_from_val, node_to_val):
-        from_found = None
-        to_found = None
+        nodes = {node_from_val: None, node_to_val: None}
         for node in self.nodes:
-            if node_from_val == node.value:
-                from_found = node
-            if node_to_val == node.value:
-                to_found = node
-        if from_found == None:
-            from_found = Node(node_from_val)
-            self.nodes.append(from_found)
-        if to_found == None:
-            to_found = Node(node_to_val)
-            self.nodes.append(to_found)
-        new_edge = Edge(new_edge_val, from_found, to_found)
-        from_found.edges.append(new_edge)
-        to_found.edges.append(new_edge)
+            if node.value in nodes:
+                nodes[node.value] = node
+                if all(nodes.values()):
+                    break
+        for node_val in nodes:
+            nodes[node_val] = nodes[node_val] or self.insert_node(node_val)
+        node_from = nodes[node_from_val]
+        node_to = nodes[node_to_val]
+        new_edge = Edge(new_edge_val, node_from, node_to)
+        node_from.edges.append(new_edge)
+        node_to.edges.append(new_edge)
         self.edges.append(new_edge)
-
+ 
     def get_edge_list(self):
-        edge_list = []
-        return edge_list
-
+        return [(e.value, e.node_from.value, e.node_to.value)
+                for e in self.edges]
+ 
+ 
     def get_adjacency_list(self):
-        adjacency_list = [None] * (len(self.nodes))
-        return adjacency_list
-    
-    
+        max_index = self.find_max_index()
+        adjacency_list = [[] for _ in range(max_index+1)]
+        for edg in self.edges:
+            from_value, to_value = edg.node_from.value, edg.node_to.value
+            adjacency_list[from_value].append((to_value, edg.value))
+        return [a or None for a in adjacency_list] 
+ 
+ 
     def get_adjacency_matrix(self):
-        adjacency_matrix = [[0 for i in range(len(self.nodes))] for j in range(len(self.nodes))]
+        max_index = self.find_max_index()
+        adjacency_matrix = [[0] * (max_index+1) for _ in range(max_index + 1)]
+        for edg in self.edges:
+            from_index, to_index = edg.node_from.value, edg.node_to.value
+            adjacency_matrix[from_index][to_index] = edg.value
         return adjacency_matrix
+ 
+    def find_max_index(self):
+        if len(self.node_names) > 0:
+            return len(self.node_names)
+        max_index = -1
+        if len(self.nodes):
+            for node in self.nodes:
+                if node.value > max_index:
+                    max_index = node.value
+        return max_index
+ 
